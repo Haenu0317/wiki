7d90e4676cdbee2c041ddd7a923b724f5d816b4a
Haenu0317 <2523053533@qq.com>
2023-10-25 11:23:16 +0800

    增加电子书管理页面

--- wiki-frontend/src/views/About.vue
+++ wiki-frontend/src/views/About.vue
@@ -1,5 +1,12 @@
 <template>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about page11111111111</h1>
   </div>
 </template>
---
+++ wiki-frontend/src/views/admin/admin-Ebook.vue
@@ -0,0 +1,7 @@
+<template>
+
+  <div class="about">
+    <h1>电子书管理</h1>
+  </div>
+</template>
+
