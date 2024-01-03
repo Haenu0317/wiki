d7dde17e5ae9c1338db4898684b07bf5aa50e5ba
Haenu0317 <2523053533@qq.com>
2024-01-03 11:56:00 +0800

    fix: footer置下

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,21 +1,48 @@
 <template>
-  <a-layout id="components-layout-demo-top-side-2">
-    <the-header></the-header>
-    <router-view/>
-    <the-footer></the-footer>
-  </a-layout>
+  <div id="app">
+    <div class="m-content-display">
+
+      <the-header/>
+      <div class="m-router-view">
+        <router-view/>
+      </div>
+      <the-footer/>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-  import { defineComponent } from 'vue';
-  import TheHeader from '@/components/the-header.vue';
-  import TheFooter from '@/components/the-footer.vue';
+import {defineComponent} from 'vue';
+import TheHeader from '@/components/the-header.vue';
+import TheFooter from '@/components/the-footer.vue';
 
-  export default defineComponent({
-    name: 'app',
-    components: {
-      TheHeader,
-      TheFooter,
-    },
-  });
+export default defineComponent({
+  name: 'app',
+  components: {
+    TheHeader,
+    TheFooter,
+  },
+});
 </script>
+
+<style>
+.m-content-display {
+  display: flex;
+  flex-direction: column;
+  min-height: 100%;
+}
+
+.m-router-view {
+  flex: 1;
+}
+
+#app {
+  /*其他*/
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+}
+
+</style>
